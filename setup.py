c19c0269aca6253e765ebb82a33ed6a8b0e757ee
chen <ink.pad@163.com>
2019-01-30 23:44:17 +0800

    wsd and wss

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='pdwind',
+    version='0.0.1',
+    author='fuchen',
+    author_email='',
+    url='https://zhuanlan.zhihu.com/p/26159930',
+    description=u'Pandas adapter for WindPy',
+    packages=['pdwind'],
+    install_requires=[],
+    entry_points={
+    }
+)
